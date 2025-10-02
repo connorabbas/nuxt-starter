2f0e08b875b17e9f98203b878c2f60a46f2687b8
Connor Abbas <abbasconnor@gmail.com>
2025-10-02 02:08:48 +0000

    wip: cleanup, working app with docker + devcontainer setup locally

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,28 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: [
-    '@nuxt/eslint',
-    '@nuxt/ui'
-  ],
-
-  devtools: {
-    enabled: true
-  },
-
-  css: ['~/assets/css/main.css'],
-
-  routeRules: {
-    '/': { prerender: true }
-  },
-
-  compatibilityDate: '2025-01-15',
-
-  eslint: {
-    config: {
-      stylistic: {
-        commaDangle: 'never',
-        braceStyle: '1tbs'
-      }
+    modules: [
+        '@nuxt/eslint',
+        '@nuxt/ui'
+    ],
+    devtools: {
+        enabled: false
+    },
+    css: ['~/assets/css/main.css'],
+    routeRules: {
+        '/': { prerender: true }
+    },
+    devServer: {
+        port: Number(process.env.NUXT_PORT) || 3000
+    },
+    compatibilityDate: '2025-01-15',
+    eslint: {
+        config: {
+            stylistic: false
+        }
     }
-  }
-})
+});
